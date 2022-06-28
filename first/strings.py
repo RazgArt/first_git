9591852c9bede581462d9ee2cdcb906ee40dd671
RazArt <jiajia@gmail.com>
2022-06-28 10:23:17 +1000

    add file strings and correct file suns

---
+++ first/strings.py
@@ -0,0 +1,8 @@
+def string(a):
+    return a*2
+
+
+
+if __name__ == '__main__':
+    print(string("pa"))
+
--- first/suns.py
+++ first/suns.py
@@ -1,7 +1,11 @@
-def sums(a,b):
-    return a+b
-def minus(a,b):
-    return a-b
+def sums(a, b):
+    return a + b
+
+
+def minus(a, b):
+    return a - b
+
+
 if __name__ == '__main__':
-    print(sums(2,2))
-    print(minus(5,2))
+    print(sums(2, 2))
+    print(minus(5, 2))
